cb1ca18a9d2b5c065634dce1aa3c11096590587d
Eizval <damian.mermod@mac.com>
2024-05-23 16:49:52 +0200

    started game of life

--- Tag6/a1.js
+++ Tag6/a1.js
@@ -1,38 +1,95 @@
 let pixel = [];
+let squareSideLenght = 20;
+let gameIsRunning = false;
 
 function setup() {
   let y;
   let x;
-  let quadres = 20;
-  if ((windowHeight / 2) % quadres === 0) {
+
+  if ((windowHeight / 2) % squareSideLenght === 0) {
     y = windowHeight / 2;
   } else {
-    let minus = (windowHeight / 2) % quadres;
+    let minus = (windowHeight / 2) % squareSideLenght;
     y = windowHeight / 2 - minus;
   }
 
-  if ((windowWidth / 2) % quadres === 0) {
+  if ((windowWidth / 2) % squareSideLenght === 0) {
     x = windowWidth / 2;
   } else {
-    let minus = (windowWidth / 2) % quadres;
+    let minus = (windowWidth / 2) % squareSideLenght;
     x = windowWidth / 2 - minus;
   }
 
   createCanvas(x, y);
   //   strokeWeight(1);
-  stroke(255, 255, 255);
-  fill(0);
+  createField();
 
-  for (let x = 0; x < width; x += quadres) {
-    pixel.push([])
-    for (let y = 0; y < height; y += quadres) {
-      square(x, y, quadres);
-      pixel.push({x,y,isAlive: false});
+  print(pixel);
+}
+
+function draw() {
+  frameRate(10);
+  //   print(pixel)
+  redrawField();
+  if (gameIsRunning) {
+    liveRules();
+  }
+  // print(gameIsRunning);
+}
+
+function mousePressed() {
+  for (let x = 0; x < pixel.length; x++) {
+    for (let y = 0; y < pixel[x].length; y++) {
+      currentSquare = pixel[x][y];
+      if (
+        currentSquare.x - 1 < mouseX &&
+        mouseX < currentSquare.x + squareSideLenght + 1 &&
+        currentSquare.y - 1 < mouseY &&
+        mouseY < currentSquare.y + squareSideLenght + 1
+      ) {
+        currentSquare.isAlive = !currentSquare.isAlive;
+      }
     }
   }
 }
 
-function draw() {
-  frameRate(1);
-//   print(pixel)
+function redrawField() {
+  clear();
+  for (let x = 0; x < pixel.length; x++) {
+    for (let y = 0; y < pixel[x].length; y++) {
+      currentSquare = pixel[x][y];
+      if (currentSquare.isAlive == true) {
+        fill(255);
+      } else {
+        fill(0);
+      }
+      square(currentSquare.x, currentSquare.y, squareSideLenght);
+    }
+  }
+}
+
+function createField() {
+  stroke(255, 255, 255);
+  fill(0);
+
+  for (let x = 0; x < width; x += squareSideLenght) {
+    pixel.push([]);
+    for (let y = 0; y < height; y += squareSideLenght) {
+      square(x, y, squareSideLenght);
+      pixel[pixel.length - 1].push({ x, y, isAlive: false });
+    }
+  }
+}
+
+function liveRules() {
+  // wenn er am rand ist dann schaut er ob es negative 
+  // ist oder ob es grösser ist als die max zahl und dann nehmt er die differenz
+  // und sucht das feld mit diesen x / y achsen und das ist dass feld dass er jetzt schauen
+  // muss ob es alive ist oder nicht
+}
+
+function keyPressed() {
+  if (keyCode === 13) {
+    gameIsRunning = !gameIsRunning;
+  }
 }
